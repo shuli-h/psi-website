cb58dfdb8e2ceb9673c86b35512411c341edde76
Shuli Hatuka <shuli150692@gmail.com>
2026-06-23 17:46:15 +0300

    Add sitemap and robots

---
+++ src/app/sitemap.js
@@ -0,0 +1,9 @@
+export default function sitemap() {
+  return [
+    { url: 'https://www.psi-safety.co.il/', lastModified: new Date() },
+    { url: 'https://www.psi-safety.co.il/about', lastModified: new Date() },
+    { url: 'https://www.psi-safety.co.il/contact', lastModified: new Date() },
+    { url: 'https://www.psi-safety.co.il/hadpasat-midadim', lastModified: new Date() },
+    { url: 'https://www.psi-safety.co.il/thank-you', lastModified: new Date() },
+  ];
+}
